18bbb005e7827b3734d65e3121583856eb2ddd70
Domonkos Czifra <doma945@gmail.com>
2022-05-18 07:10:38 +0000

    Updated triangle area :(

--- scripts/calc.py
+++ scripts/calc.py
@@ -18,24 +18,21 @@ def calc1():
         triangle = (0.5*x)/2-get_segment(gamma)
 
         covered_area = segment_hor+2*segment_ver+trapez+2*triangle
-        all_area_inf = 1/(2*np.sqrt(2))
+        all_area_inf = 1/(2*np.sqrt(3))
 
         # If we place the pattern 60 deg below th others, than x should be:
         x_should = 0.5*np.tan(np.pi/6)
         if(np.abs(x-x_should) < 0.00001):
-            base = 2
-            new = 1+np.cos(gamma/2)
-            ratio = base/new
-            print(covered_area)
-            covered_area = 0.6774
-
-            print(2*180*beta/np.pi)
+            new_area = np.sqrt((2*np.cos(gamma/2))**2-1)*2
+            #new_area = np.sqrt(3)*2
+            #print(new_area*2)
+            #print(np.sqrt(3)*2)
 
             #print(np.cos(gamma/2), base/new)
             #print(segment_hor, segment_ver, trapez, triangle, "Final", ratio*covered_area*all_area_inf)
             #print(f"Alpha: {alpha:.2f} beta: {beta:.2f} gamma: {gamma:.2f}  [x:{x}]==> Final 0: {covered_area*all_area_inf:.4f} [advanced: {ratio*covered_area*all_area_inf:.4f}]")
             
-            print(f"Alpha: {alpha} beta: {beta} gamma: {gamma}  [x:{x}]==> Final 0: {covered_area*all_area_inf:.4f} [advanced: {ratio*covered_area*all_area_inf:.4f}]")
+            print(f"Alpha: {alpha} beta: {beta} gamma: {gamma}  [x:{x}]==> Final 0: {covered_area*all_area_inf:.4f} [advanced: {covered_area/new_area:.4f}]")
 
 def calc2():
     pass
